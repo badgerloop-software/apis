0568e46b995c1db13a8af43243239c13f5c550e2
vkottler <vkottler@wisc.edu>
2017-09-10 23:49:49 -0500

    intermediate commit, going to bed. Almost got a somewhat finalized ERD

--- php/members.php
+++ php/members.php
@@ -1,5 +1,15 @@
 <?php
 
-echo "Members tab<br>";
+$tier_query = "SELECT name, value FROM `tier`;";
+if ($result = $conn->query($tier_query)) {
+
+	while ($row = $result->fetch_object()) {
+		$data[] = $row;
+		var_dump($row);
+		echo "<br>";
+	}
+
+	$result->close();
+}
 
 ?>
---
+++ sql_scripts/tier.sql
@@ -0,0 +1,3 @@
+INSERT INTO `website-data`.`tier` (`name`,`value`) VALUES ("Team Member","0");
+INSERT INTO `website-data`.`tier` (`name`,`value`) VALUES ("Team Lead","1");
+INSERT INTO `website-data`.`tier` (`name`,`value`) VALUES ("Executive","2");
