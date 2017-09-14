bd1b94841bd8876cb50b9872503867609d68e653
Jordan Jones <jordanalanjones@gmail.com>
2017-09-13 23:05:31 -0500

    Changing members files to return members, not tiers. Also adding new teirs file. Changed output to output JSON for easier parsing in Angular

--- php/members.php
+++ php/members.php
@@ -1,15 +1,16 @@
 <?php
+require_once 'database.php';
 
 $tier_query = "SELECT name, value FROM `tier`;";
 if ($result = $conn->query($tier_query)) {
 
-	while ($row = $result->fetch_object()) {
-		$data[] = $row;
-		var_dump($row);
-		echo "<br>";
-	}
+    while ($row = $result->fetch_object()) {
+        $data[] = $row;
+        var_dump($row);
+        echo "<br>";
+    }
 
-	$result->close();
+    $result->close();
 }
 
 ?>
