04af43a83b755c089c95bd05790d7e882c04cea0
Jordan Jones <jordanalanjones@gmail.com>
2017-09-17 11:00:19 -0500

    Editing area API to not take a paramater, was not needed and would just cause more code. Also adding code to allow API to be called from different sites

--- php/area.php
+++ php/area.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 require_once 'database.php';
 
-$member_query = "SELECT a.name as `area_name`, t.name, t.description FROM `area` a, `team` t where a.id = t.area and a.name = '" . $_GET['area'] . "'";
+$member_query = "SELECT a.name as `area_name`, t.name, t.description FROM `area` a, `team` t where a.id = t.area";
 if ($result = $conn->query($member_query)) {
 
 	$data = array();
