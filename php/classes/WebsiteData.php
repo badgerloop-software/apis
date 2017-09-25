5947e874747420092014d16bd78ae2a04876b6f7
Vaughn Kottler <vkottler@wisc.edu>
2017-09-24 23:47:16 -0500

    trying to sync work

--- php/classes/WebsiteData.php
+++ php/classes/WebsiteData.php
@@ -14,9 +14,7 @@ class WebsiteData {
 		}
 	}
 
-	function __destruct() {
-		$this->conn->close();
-	}
+	function __destruct() { $this->conn->close(); }
 
 	function getTiers() {
 
@@ -63,6 +61,20 @@ class WebsiteData {
 		$data = array();
 
 		$member_query = "SELECT name, level FROM `position`;";
+		if ($result = $this->conn->query($member_query)) {
+			while ($row = $result->fetch_object())
+				$data[] = $row;
+			$result->close();
+		}
+
+		return $data;
+	}
+
+	function getTeam() {
+
+		$data = array();
+
+		$member_query = "select concat(m.`first_name`, \" \", m.`last_name`) as full_name , m.`email`, m.`headshot`, p.`name` as position_name, t.`name` as tier_name, y.`name` as `year` , mj.`name` as `major` from `member` m, `position` p, `tier` t, `year` y, `major` mj where m.`position` = p.`id` and p.`level` = t.`id` and y.`id` = m.year and mj.id = m.major";
 		if ($result = $this->conn->query($member_query)) {
 			while ($row = $result->fetch_object())
 				$data[] = $row;
